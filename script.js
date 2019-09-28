3a403238bbbe29c1d80d0f9ce1e9126a102ecdb9
nirvanaczj <ns_chenzijia@aol.com>
2019-09-27 21:24:37 -0400

    sep27 update

--- script.js
+++ script.js
@@ -1,24 +1,57 @@
+var cursor = document.querySelector('.cursor')
+
+var cursorMove = function(event)
+{
+	//console.log(event.pageX)
+	cursor.style.left = `${event.pageX - 8}px`
+	cursor.style.top = `${event.pageY - 8}px`
+	//document.body.insertAdjacentHTML('beforeend',`<div style="width:${event.pageX}px"></div>`)
+}
+
+window.addEventListener('mousemove',cursorMove)
+
+
+var submitBtn = document.querySelector('input[type=submit]')
+
+console.log(submitBtn.style.display)
+
+var showsubmitBtn = function(str){
+    if (str.length === 0){
+    submitBtn.style.display = 'none';
+}   else{
+    console.log('triggered')
+    submitBtn.style.display = 'block';
+}
+}
+
+
 function myFunction() {
   event.preventDefault();
+  $('form').fadeOut(0);
+  var body = document.body;
+  body.style.margin = '0px';
+  cursor.style.backgroundColor ='cornsilk';
   var text1 = document.getElementById("text1").value;
   var text2 = document.getElementById("text2").value;
-  $('form').fadeOut(500);
- 
   //$('.wrapper').addClass('form-success'); 
     
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange=function()
   {
-    if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    //if (xmlhttp.readyState==4 && xmlhttp.status==200)
     { 
       var result_array =  JSON.parse(xmlhttp.responseText).result;
-        
+          var test_array = [];
+          var test_array_2 = [];
+          for(a = 0; a < 1024; a++){
+              test_array.push(Math.random())
+              test_array_2.push(Math.random())}
           var result_1 = document.createElement("div");
           result_1.id ="result_1";
           result_1.innerHTML ="This is the result of text 1";
           document.getElementById("result").appendChild(result_1);
           var first_text_encoded = result_array[0];
-        
+          //var first_text_encoded = test_array;
         
           for (j = 0; j < first_text_encoded.length; j++){
           var result_1_0 = document.createElement("div");
@@ -34,6 +67,7 @@ function myFunction() {
           result_2.innerHTML ="This is the result of text 2";
           document.getElementById("result").appendChild(result_2);
           var second_text_encoded = result_array[1];
+          //var second_text_encoded = test_array_2
           //  console.log(second_text_encoded.length);
         
           var arr_euclidean_element = [];
@@ -51,8 +85,8 @@ function myFunction() {
           var arr_diff_pow_visual = Math.pow(arr_diff,2)*200;
           arr_euclidean_element.push(arr_diff_pow)
           console.log(arr_diff_pow_visual);
-          var arr_diff_visual = `<div id="visual", style="display：inline-block;width:20px;height:${arr_diff_pow_visual}px;background-color: rgb(${200-k}, ${50+k}, ${100-k});"></div>`;
-          document.getElementById("visualization").insertAdjacentHTML('beforeend', arr_diff_visual);
+          var arr_diff_visual = `<div id="visual", style="display：inline-block;width:3px;height:${arr_diff_pow_visual*4}px;background-color: rgb(${100+arr_diff_pow_visual}, 164, 169);"></div>`;
+          document.getElementById("visualization").insertAdjacentHTML('beforeend', arr_diff_visual);}
           }
     console.log(arr_euclidean_element);
     var sum_euclidean = 0;
@@ -61,11 +95,21 @@ function myFunction() {
     console.log(sum_euclidean);
     var arr_euclidean = Math.sqrt(sum_euclidean);
     console.log("The Euclidean distance of this two sentences is :",arr_euclidean);   
-    var euclidean = `<h2 style="font-family:romans;font-style:italic">The Euclidean distance of this two sentences is :${arr_euclidean}.</h2>`;    
-    document.getElementById("visualization").insertAdjacentHTML('beforebegin', euclidean);}
+    var euclidean = `<h2 style="margin-left:5px;position:fixed;left:0px;">The Euclidean distance of this two sentences is :${arr_euclidean}.</h2>`; 
+    var euclidean_str = `The Euclidean distance of this two sentences is :${arr_euclidean}.` 
+    var b = 0;
+    function typeWritter(){
+        if(b < euclidean_str.length){
+            document.getElementById('euclidean').innerHTML += euclidean_str(b);
+            b++;
+            setTimeout(typeWritter,50);
+        }
+    }
+    
+    document.getElementById("visualization").insertAdjacentHTML('beforebegin', euclidean);
 
   }
-  xmlhttp.open("POST","http://api.zijiachen.com:11233/encode",true);
+  xmlhttp.open("POST","http://35.196.190.127:11233/encode",true);
   xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   xmlhttp.send(JSON.stringify({"id": 1,"texts": [text1,text2], "is_tokenized": false}));   
 };
